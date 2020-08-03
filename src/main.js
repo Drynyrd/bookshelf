ca6f526e41661ba34dc140905972f2e60e0209a2
Drynyrd <ferro.tim@gmail.com>
2020-08-04 01:05:19 +0200

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,18 @@
+import Vue from "vue";
+import App from "./App.vue";
+import Vuetify from "vuetify";
+import router from "./router";
+import "vuetify/dist/vuetify.min.css";
+
+const opts = {
+  icons: {
+    iconfont: "mdi"
+  }
+};
+Vue.use(Vuetify);
+
+new Vue({
+  router,
+  vuetify: new Vuetify(opts),
+  render: h => h(App)
+}).$mount("#app");
---
+++ src/views/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1>Home</h1>
+    <v-card class="mx-8">
+      <v-data-table :headers="headers" :items="books" :items-per-page="20" class="elevation-1"></v-data-table>
+    </v-card>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+const BOOKS_URL = "https://drynyrd-bookshelf.builtwithdark.com/books";
+
+export default {
+  data: function() {
+    return {
+      headers: [{ text: "id", value: "id" }, { text: "name", value: "name" }],
+      books: []
+    };
+  },
+  methods: {
+    fetchBooks: function() {
+      axios.get(BOOKS_URL).then(response => (this.books = response.data));
+    }
+  },
+  mounted: function() {
+    this.fetchBooks();
+  }
+};
+</script>
+
+<style>
+</style>
